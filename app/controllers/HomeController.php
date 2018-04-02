67f19eb488342778f46c8e92726f62dbe0be5658
Bucky85 <zimmer.clementpro@gmail.com>
2018-04-02 10:50:32 +0200

    - minim correct

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -11,6 +11,7 @@ class HomeController
 {
     public function home(Request $request, Response $response)
     {
-        return Utils::update_response($response, 200)->withRedirect("http://" . $_SERVER['SERVER_NAME']);
+        return Utils::update_response($response, 200)
+            ->withRedirect("http://" . $_SERVER['SERVER_NAME']);
     }
 }
